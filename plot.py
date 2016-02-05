9d21e4449fc680e8e2c6017da047d5a8540773d3
logicabrity <mail@marc-antonio.de>
2016-02-05 13:23:58 +0000

    discover data for plots, found bug

--- plot.py
+++ plot.py
@@ -1,5 +1,7 @@
-import os
+import glob
 import numpy as np
+import os
+import re
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -8,17 +10,17 @@ from fidimag.common.fileio import DataReader  # for convenience
 plt.style.use("ggplot")
 
 # input files and data from standard problem 4 field 2 simulations
-
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 OOMMF_DATAFILE = os.path.join(MODULE_DIR, "oommf", "std_4_dynamics_field_2.txt")
-FIDIMAG_DATAFILE = os.path.join(MODULE_DIR, "fidimag", "dyn_r{0}_a{0}.txt")
-fidimag_tols = xrange(5, 11)
+FIDIMAG_DATAFILES = sorted(glob.glob(os.path.join(MODULE_DIR, "fidimag", "dyn_r*_a*.txt")))
 oommf_data = np.loadtxt(OOMMF_DATAFILE)
 
+#FIXME: oommf magnetisation data is recorded between t = 4e-12 and t = 2.004e-9
+# whereas fidimag data is recorded between t = 0 and t = 2e-9 (as it should be)
+# that explains the slight discrepancy (?)
 
 # LaTeX labels
-
-math = lambda txt: "$" + txt + "$"  # concatenation because getting {{{}}}
+math = lambda txt: "$" + txt + "$"      # concatenation because getting {{{}}}
 rm = lambda txt: "\mathrm{" + txt + "}" # right in subsequent formats is hard
 olbl = math(rm("oommf"))
 flbl = math(rm("fidimag"))
@@ -26,72 +28,39 @@ lbli = lambda name, mi: math(rm(rm(name) + "\; m_" + rm(mi)))
 olbli = lambda i: lbli("oommf", i)
 flbli = lambda i: lbli("fidimag", i)
 
+for fi in FIDIMAG_DATAFILES:
+    data = DataReader(fi)
+    tol = re.search('dyn_r([0-9]+)_a', fi).groups()[0]  # get exponent from filename
+    difference = np.sqrt((oommf_data[:, 2] - data["m_x"]) ** 2 +
+                         (oommf_data[:, 3] - data["m_y"]) ** 2 +
+                         (oommf_data[:, 4] - data["m_z"]) ** 2)
 
-# plotting
+    fig, axes = plt.subplots(3, figsize=(10, 8), sharex= True)
 
-# showing averages
+    # dynamics of average magnetisation plotted
+    axes[0].plot(oommf_data[:, 0] * 1e9, oommf_data[:, 2], "b-", label=olbli("x"))
+    axes[0].plot(oommf_data[:, 0] * 1e9, oommf_data[:, 3], "g-", label=olbli("y"))
+    axes[0].plot(oommf_data[:, 0] * 1e9, oommf_data[:, 4], "r-", label=olbli("z"))
+    axes[0].plot(data["time"] * 1e9, data["m_x"], "bx", label=flbli("x"))
+    axes[0].plot(data["time"] * 1e9, data["m_y"], "gx", label=flbli("y"))
+    axes[0].plot(data["time"] * 1e9, data["m_z"], "rx", label=flbli("z"))
+    axes[0].set_ylim((-1.05, 1))
+    axes[0].set_ylabel("unit magnetisation (1)")
+    axes[0].legend()
+    axes[0].set_title("tol 1e-{}".format(tol))
 
-fig, axes = plt.subplots(len(fidimag_tols), figsize=(10, 8), sharex=True)
-axes[-1].set_xlabel("time (ns)")
-axes[-1].set_xlim((0, 2))
+    # difference of average magnetisations between oommf and finmag
+    axes[1].plot(data["time"] * 1e9, difference, label=str(tol))
+    axes[1].set_ylabel("difference")
 
-for i, tol in enumerate(fidimag_tols):
-    fidimag_data = DataReader(FIDIMAG_DATAFILE.format(tol))
-    axes[i].plot(oommf_data[:, 0] * 1e9, oommf_data[:, 2], "b-", label=olbli("x"))
-    axes[i].plot(oommf_data[:, 0] * 1e9, oommf_data[:, 3], "g-", label=olbli("y"))
-    axes[i].plot(oommf_data[:, 0] * 1e9, oommf_data[:, 4], "r-", label=olbli("z"))
-    axes[i].plot(fidimag_data["time"] * 1e9, fidimag_data["m_x"], "bx", label=flbli("x"))
-    axes[i].plot(fidimag_data["time"] * 1e9, fidimag_data["m_y"], "gx", label=flbli("y"))
-    axes[i].plot(fidimag_data["time"] * 1e9, fidimag_data["m_z"], "rx", label=flbli("z"))
-    axes[i].set_ylim((-1.05, 1))
-    axes[i].set_ylabel("unit magnetisation (1)")
-    axes[i].legend()
-    axes[i].set_title("tol 1e-{}".format(tol))
+    # number of RHS evaluations
+    axes[2].plot(oommf_data[:, 0] * 1e9, oommf_data[:, 1], "r", label=olbl)
+    axes[2].plot(data["time"] * 1e9, data["rhs_evals"], label=flbl + "$\,1e-{}$".format(tol))
+    axes[2].legend()
+    axes[2].set_ylabel("# of RHS evaluations")
+    axes[2].set_xlabel("time (ns)")
+    axes[2].set_xlim((0, 2))
 
-fig.tight_layout()
-fig.savefig("dynamics.png")
-plt.close(fig)
-
-# difference of averages between oommf and finmag
-
-fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-ax2.set_xlabel("time (ns)")
-ax2.set_xlim((0, 2))
-
-differences = [] # differences between oommf and given fidimag tol
-
-for i, tol in enumerate(fidimag_tols):
-    fidimag_data = DataReader(FIDIMAG_DATAFILE.format(tol))
-    difference = np.sqrt((oommf_data[:, 2] - fidimag_data["m_x"]) ** 2 +
-                         (oommf_data[:, 3] - fidimag_data["m_y"]) ** 2 +
-                         (oommf_data[:, 4] - fidimag_data["m_z"]) ** 2)
-    differences.append(difference)
-    ax1.plot(fidimag_data["time"] * 1e9, difference, label=str(tol))
-
-
-ax2.plot(fidimag_data["time"] * 1e9, differences[1] - differences[0])
-ax1.set_title("$\mathrm{difference\; to\; oommf}$")
-ax2.set_title("$\mathrm{difference\; of\; difference}$")  # ...
-ax1.set_ylabel("deviance (1)")
-ax2.set_ylabel("difference (1)")
-ax1.legend()
-fig.tight_layout()
-fig.savefig("difference.png")
-plt.close(fig)
-
-# number of RHS evaluations
-
-fig, axis = plt.subplots()
-axis.set_xlabel("time (ns)")
-axis.set_xlim((0, 2))
-axis.set_ylabel("# of RHS evaluations")
-
-axis.plot(oommf_data[:, 0] * 1e9, oommf_data[:, 1], "r", label=olbl)
-
-for tol in fidimag_tols:
-    fidimag_data = DataReader(FIDIMAG_DATAFILE.format(tol))
-    axis.plot(fidimag_data["time"] * 1e9, fidimag_data["rhs_evals"], label=flbl + "$\,1e-{}$".format(tol))
-
-axis.legend()
-fig.savefig("work.png")
-plt.close(fig)
+    fig.tight_layout()
+    fig.savefig("dynamics_r{}.png".format(tol))
+    plt.close(fig)
